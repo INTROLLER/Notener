5fbc3b00c78d3287757d98454d68200eacfeea4f
Pavel Vorozhun <92254906+INTROLLER@users.noreply.github.com>
2023-08-05 03:17:48 +0200

    Update root.py

--- root.py
+++ root.py
@@ -8,6 +8,7 @@ window.title("Notener")
 start_width = 650
 window.geometry(f"{start_width}x400")
 amount_of_notes = 0
+amount_of_elements = 0
 
 notespath = 'Notes\\'
 
@@ -21,6 +22,8 @@ if not isExist:
 
 
 file_data = {}
+widgets = []
+
 
 # Iterate over all files in the folder
 for filename in os.listdir(notespath):
@@ -42,34 +45,51 @@ for filename, content in file_data.items():
     print()
 
 #load all notes at start
-for file_name_without_extension, content in file_data.items():
-    if amount_of_notes <= 3:
-        y_value = 0.1412 + (0.24 * amount_of_notes)
-        x_value = 0.51
-    elif amount_of_notes >= 4:
-        y_value = 0.1412 + (0.24 * amount_of_notes) - 0.96
-        x_value = 0.83
+def load():
+    global amount_of_notes
+    global amount_of_elements
+    global NewNoteTitle
+    global NewNoteDescription
 
-    NewTitle = file_name_without_extension
-    NewDescription = content
-    NewNoteFrame = CTkFrame(window)
-    NewNoteFrame.place(relx=x_value, rely=y_value, relwidth=0.29, relheight= 0.23, anchor="center")
+    for file_name_without_extension, content in file_data.items():
+        if amount_of_notes <= 3:
+            y_value = 0.1412 + (0.24 * amount_of_notes)
+            x_value = 0.51
+        elif amount_of_notes >= 4:
+            y_value = 0.1412 + (0.24 * amount_of_notes) - 0.96
+            x_value = 0.83
+        
+        NewTitle = file_name_without_extension
+        NewDescription = content
+        NewNoteFrame = CTkFrame(window)
+        NewNoteFrame.place(relx=x_value, rely=y_value, relwidth=0.29, relheight= 0.23, anchor="center")
 
-    NewNoteDescription = CTkTextbox(NewNoteFrame, fg_color="transparent", padx=5, pady=5, height=75, activate_scrollbars=False, wrap="word")
-    NewNoteDescription.place(relx=0.5, rely=0.6, anchor="center")
-    NewNoteDescription.insert("0.0", f"{NewDescription}")
-    NewNoteDescription.configure(state="disabled")
+        NewNoteDescription = CTkTextbox(NewNoteFrame, fg_color="transparent", padx=5, pady=5, height=75, activate_scrollbars=False, wrap="word")
+        NewNoteDescription.place(relx=0.5, rely=0.6, anchor="center")
+        NewNoteDescription.insert("0.0", f"{NewDescription}")
+        NewNoteDescription.configure(state="disabled")
 
-    NewNoteTitle = CTkLabel(NewNoteFrame, text=NewTitle, font=("Outfit", 20, "bold"))
-    NewNoteTitle.place(relx=0.5, rely=0.15, anchor="center")
-    
-    NewNoteDescScrollBar = CTkScrollbar(NewNoteFrame, height=0.01)
-    NewNoteDescription.configure(yscrollcommand=NewNoteDescScrollBar.set)
-    amount_of_notes += 1
+        NewNoteTitle = CTkLabel(NewNoteFrame, text=NewTitle, font=("Outfit", 20, "bold"))
+        NewNoteTitle.place(relx=0.5, rely=0.15, anchor="center")
+        
+        NewNoteDescScrollBar = CTkScrollbar(NewNoteFrame, height=0.01)
+        NewNoteDescription.configure(yscrollcommand=NewNoteDescScrollBar.set)
+
+        widgets.append(NewNoteFrame)
+
+        amount_of_notes += 1
+        amount_of_elements += 1
+
+load()
 
 #commands
 def save_new_note1():
     global amount_of_notes
+    global amount_of_elements
+    global NewNoteTitle
+    global NewNoteDescription
+    global NewNoteFrame
+
     NewTitle = TitleEntry.get()
     NewDescription = ContentEntry.get()
     if NewTitle == "":
@@ -90,7 +110,7 @@ def save_new_note1():
         NewNoteDescription = CTkTextbox(NewNoteFrame, fg_color="transparent", padx=5, pady=5, height=75, activate_scrollbars=False, wrap="word")
         NewNoteDescription.place(relx=0.5, rely=0.6, anchor="center")
         NewNoteDescription.insert("0.0", f"{NewDescription}")
-        NewNoteDescription.configure(state="disabled", justify=CENTER)
+        NewNoteDescription.configure(state="disabled")
 
         NewNoteTitle = CTkLabel(NewNoteFrame, text=NewTitle, font=("Outfit", 20, "bold"))
         NewNoteTitle.place(relx=0.5, rely=0.15, anchor="center")
@@ -105,6 +125,10 @@ def save_new_note1():
         Button1.configure(text="Create New Note", command=open_creating_settings, fg_color="#1c94ff", hover_color="#0059a8", text_color="#000000")
         Button1.place(relx=0.18, rely=0.45)
         Button2.place(relx=0.18, rely=0.55)
+
+        widgets.append(NewNoteFrame)
+
+        amount_of_elements += 1
 
 def open_creating_settings():
     global amount_of_notes
@@ -125,8 +149,16 @@ def open_creating_settings():
     #int(New_Name).place(relx=0.18, rely=0.2, relwidth=0.3, relheight= 0.08, anchor="center")
     #print(New_Name)
 
+def remove():
+    for NewNoteFrame in widgets:
+        NewNoteFrame.destroy()
+
 def delete_all_notes():
-    print("Deleted")
+    global amount_of_notes
+    for f in os.listdir(notespath):
+        os.remove(os.path.join(notespath, f))
+    remove()
+    amount_of_notes = 0
 
 
 #entitites
@@ -136,7 +168,6 @@ Button2.place(relx=0.18, rely=0.6, relwidth=0.3, anchor="center")
 Label1 = CTkLabel(window, text="Press")
 Label2 = CTkLabel(window, text="Press")
 Entry1 = CTkEntry(window)
-
 
 #place entities
 Button1.place(relx=0.18, rely=0.45, relwidth=0.3, relheight= 0.08, anchor="center")
